dcc47586388945752501a182261bb2b49f28d52f
wisdomx2 <wisdomx2@hanmail.net>
2023-05-17 18:08:59 +0900

    0517

---
+++ final-pjt-front/client/src/components/PopularView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>인기순위</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PopularView",
+};
+</script>
+
+<style></style>
---
+++ final-pjt-front/client/src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/Reelix.png" />
+    <MainView />
+    <TopRatedView />
+    <PopularView />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import MainView from "@/components/MainView.vue";
+import PopularView from "@/components/PopularView.vue";
+import TopRatedView from "@/components/TopRatedView.vue";
+
+export default {
+  name: "HomeView",
+  components: {
+    MainView,
+    PopularView,
+    TopRatedView,
+  },
+};
+</script>
